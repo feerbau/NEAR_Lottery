b0409ddbe9bd7c1349fbe1b7da0b11ca8550e916
feerbau <fermin-lp@hotmail.com>
2022-04-29 13:35:22 -0300

    Test new

--- src/lib.rs
+++ src/lib.rs
@@ -58,15 +58,74 @@ impl Lottery {
     }
 
     pub fn get_participans(&self) -> std::vec::Vec<AccountId>{
-        let par = self.participants.to_vec();
-        return par;
+        return self.participants.to_vec();
     }
 
-    pub fn get_end_date(&mut self) -> Duration {
+    pub fn get_num_participans(&self) -> u64{
+        return self.participants.len();
+    }
+
+    pub fn get_end_date(&self) -> Duration {
         return self.end_date;
     }
+
+    pub fn get_lottery_name(&self) -> String {
+        return self.lottery_name.clone();
+    }
     
-    pub fn get_winner(&mut self) -> AccountId {
-        
+    // pub fn get_winner(&mut self) -> AccountId {
+    //     assert!(assert_end(self.end_date), "Not finished");
+    // }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+#[cfg(test)]
+mod tests{
+    use near_sdk::MockedBlockchain;
+    use near_sdk::{testing_env, VMContext};
+    use std::panic;
+    use super::*;
+
+    fn catch_unwind_silent<F: FnOnce() -> R + panic::UnwindSafe, R>(
+        f: F,
+    ) -> std::thread::Result<R> {
+        let prev_hook = panic::take_hook();
+        panic::set_hook(Box::new(|_| {}));
+        let result = panic::catch_unwind(f);
+        panic::set_hook(prev_hook);
+        result
+    }
+
+    fn get_context() -> VMContext {
+        VMContext {
+            current_account_id: "alice".to_string(),
+            signer_account_id: "bob".to_string(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id: "bob".to_string(),
+            input: vec![],
+            block_index: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage: 10u64.pow(6),
+            attached_deposit: 0,
+            prepaid_gas: 10u64.pow(15),
+            random_seed: vec![0, 1, 2],
+            is_view: false,
+            output_data_receivers: vec![],
+            epoch_height: 0,
+        }
+    }
+
+    #[test]
+    fn test_new() {
+        let context = get_context();
+        testing_env!(context);
+        let lottery_name = "Test".to_string();
+        let end_date = 1651249778;
+        let contract = Lottery::new(lottery_name.clone(), end_date);
+        assert_eq!(contract.get_lottery_name(), lottery_name);
+        assert_eq!(contract.get_end_date(), end_date);
+        assert_eq!(contract.get_num_participans(), 0);
     }
 }
